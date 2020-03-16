44d66434e56b9a60134be3c2e6b7f4ce4c352a21
Mustafa Senturk <msdk45@gmail.com>
2020-03-16 00:13:57 -0400

    commit 8

--- src/gitPackage/NextClass.java
+++ src/gitPackage/NextClass.java
@@ -2,8 +2,9 @@ package gitPackage;
 
 public class NextClass {
 	
+	public static void main(String[] args) {
 	
-	System.out.println("Hello dear friends");
-
+		System.out.println("Hello dear friends");
+	}
 	
 }
